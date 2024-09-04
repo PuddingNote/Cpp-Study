061b2fd5f67b183e7bcb0a156fc4f3e1298813f8
Pudding <122201777+PuddingNote@users.noreply.github.com>
2024-09-04 15:47:08 +0900

    Add Calculator
    1. CLLBaseStack.c Complete
    2. Add Chapter06 Calculator Code

--- Cpp/Cpp/CLLBaseStack.c
+++ Cpp/Cpp/CLLBaseStack.c
@@ -5,72 +5,34 @@
 
 void StackInit(Stack* pstack)
 {
-
-
+	pstack->plist = (List*)malloc(sizeof(List));
+	ListInit(pstack->plist);
 }
 
 int SIsEmpty(Stack* pstack)
 {
-
-
-	return;
+	if (LCount(pstack->plist) == 0)
+		return TRUE;
+	else
+		return FALSE;
 }
 
 void SPush(Stack* pstack, Data data)
 {
-
-
+	LInsertFront(pstack->plist, data);
 }
 
 Data SPop(Stack* pstack)
 {
-
-
-	return;
+	Data data;
+	LFirst(pstack->plist, &data);
+	LRemove(pstack->plist);
+	return data;
 }
 
 Data SPeek(Stack* pstack)
 {
-
-
-	return;
+	Data data;
+	LFirst(pstack->plist, &data);
+	return data;
 }
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include "CLinkedList.h"
-//#include "CLLBaseStack.h"
-//
-//void StackInit(Stack* pstack)
-//{
-//	pstack->plist = (List*)malloc(sizeof(List));
-//	ListInit(pstack->plist);
-//}
-//
-//int SIsEmpty(Stack* pstack)
-//{
-//	if (LCount(pstack->plist) == 0)
-//		return TRUE;
-//	else
-//		return FALSE;
-//}
-//
-//void SPush(Stack* pstack, Data data)
-//{
-//	LInsertFront(pstack->plist, data);
-//}
-//
-//Data SPop(Stack* pstack)
-//{
-//	Data data;
-//	LFirst(pstack->plist, &data);
-//	LRemove(pstack->plist);
-//	return data;
-//}
-//
-//Data SPeek(Stack* pstack)
-//{
-//	Data data;
-//	LFirst(pstack->plist, &data);
-//	return data;
-//}
